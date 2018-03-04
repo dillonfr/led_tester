7052d1912b7d8e2e1f52d11bf47ad25084ce684c
Dillon Friel <dillonfriel@gmail.com>
2018-03-04 20:52:34 +0000

    adding basic structure with a3 slides given

--- led_tester/led_tester.py
+++ led_tester/led_tester.py
@@ -1,3 +1,33 @@
 # -*- coding: utf-8 -*-
+class LightTester:
+    Lights = None
+    
+    def __init__(self, N):
+        self.lights = [[False]*N for _ in range(N)] #creates multidimensional array with size N
+        
+    def apply(self, cmd):
+        if cmd is "turn on":
+            pass;
+        elif cmd is "turn off":
+            pass;
+        elif cmd is "switch":
+            pass;
+    
+    def count(self):
+        #count the lights that are on
+        return count
+            
+
 
 """Main module."""
+import cmd
+def main(filename, N):
+    Lights = LightTester(N)
+    
+    instructions = parseFile(filename)
+    
+    for cmd in instructions:
+        lights.apply(cmd)
+                     
+    print("# occupied: #", light.count())
+    
--- led_tester/utils.py
+++ led_tester/utils.py
@@ -22,7 +22,7 @@ def parseFile(input):
 
 def test_read_file():
     ifile = "./data/test_data.txt"
-    N, instructions = utils.parseFile(ifile)
+    N, instructions = parseFile(ifile) #utils.parseFile(ifile)
     assert N == 10
     assert instructions == ['turn on 0,0 through 9,9\n', 'turn off 0,0 through 9,9\n', 
                             'switch 0,0 through 9,9\n', 'turn off 0,0 through 9,9\n', 
