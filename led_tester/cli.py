6e1828cbb9614f448e260b2d861e779018ea6acf
Dillon Friel <dillonfriel@gmail.com>
2018-03-06 20:13:38 +0000

    added working class methods, program works on test_data.txt

--- led_tester/cli.py
+++ led_tester/cli.py
@@ -8,13 +8,29 @@ class LightTester:
         self.lights = [[False]*N for _ in range(N)] #creates multidimensional array with size N
         print(self.lights)
         
-    def apply(self, cmd):
-        if cmd is "turn on":
-            pass;
-        elif cmd is "turn off":
-            pass;
-        elif cmd is "switch":
-            pass;
+    def apply(self, cmd, x1, y1, x2, y2):
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        print("Params: ", cmd, x1, y1, x2, y2)
+        
+        if cmd == "turn on":
+            for i in range(x1, x2+1):
+                for j in range(y1, y2+1):
+                    self.lights[i][j] = True
+            print(self.lights)
+        
+        elif cmd == "turn off":
+            for i in range(x1, x2+1):
+                for j in range(y1, y2+1):
+                    self.lights[i][j] = False
+                    
+        elif cmd == "switch":
+            for i in range(x1, x2+1):
+                for j in range(y1, y2+1):
+                    if self.lights[i][j] == True:
+                        self.lights[i][j] = False
+                    else:
+                        self.lights[i][j] = True
+        return self.lights
     
     def count(self):
         count = 0
@@ -29,6 +45,7 @@ import sys
 sys.path.append('.')
 import click
 import utils
+import re
 click.disable_unicode_literals_warning = True
 
 @click.command()
@@ -47,10 +64,17 @@ def main(input=None):
     print("N is: ", N)
     for instruction in instructions:
         print(instruction)
+        pat = re.compile(".*(turn on|turn off|switch)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*")
+        cmd = pat.match(instruction)
+        print("matched: ", cmd is not None, cmd.groups())
+        print(cmd.group(1), cmd.group(2), cmd.group(3), cmd.group(4), cmd.group(5))
+        Lights.apply(cmd.group(1), cmd.group(2), cmd.group(3), cmd.group(4), cmd.group(5))
         #Lights.apply(instruction)
         
         
     print("# occupied:", Lights.count())
+    
+    
     return 0
 
 
